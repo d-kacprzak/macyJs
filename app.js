5e45a5c10b18ffd6c1c6416b635c5348cfc0d227
d-kacprzak <d.kacprzak@hotmail.com>
2019-08-29 11:56:38 +0200

    final v

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
-const macy = new Macy({
+const masonary = new Macy({
     container: '.container',
-    mobileFirst: true,
+    mobileFirs: true,
     margin: {
-        x: 100,
+        x: 20,
         y: 20
+    },
+    breakAt: {
+        568: {
+            margin: {
+                x: 10,
+                y: 10
+            },
+            columns: 1
+        },
+        768: {
+            margin: {
+                x: 10,
+                y: 10
+            },
+            columns: 2
+        },
+        992: 3,
+        1200: 4
     }
 });
+
+masonary.addEventListener('resize', function () {
+    masonary.recalculate(true, true);
+});
+
+
+
